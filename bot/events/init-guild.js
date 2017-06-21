b453f9d2a48c4638839c7a9a511854c03c4c1c1c
DarylPinto <daryl.j.pinto@gmail.com>
2017-06-20 21:18:06 -0400

    Update manager ids when guild is joined for the first time

--- bot/events/init-guild.js
+++ bot/events/init-guild.js
@@ -1,5 +1,6 @@
 const rp = require('request-promise');
 const covert = require('../../covert.js');
+const updateGuildInfo = require('../events/update-guild-info.js');
 
 module.exports = function(guild, bot_auth){
 
@@ -30,6 +31,7 @@ Type \`${prefix}help\` to get started.
 	.then(() => {
 		console.log(`Guild ${guild.id} added!`);
 		guild.defaultChannel.send(joinMessage('$'));
+		updateGuildInfo(guild, bot_auth);
 	})
 	.catch(err => {
 		
