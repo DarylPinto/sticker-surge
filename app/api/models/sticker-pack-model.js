d98351380104da0cd363d430db5f2297f7cf9911
Daryl Pinto <daryl.j.pinto@gmail.com>
2017-08-14 23:12:33 -0400

    Update sticker-pack model

--- app/api/models/sticker-pack-model.js
+++ app/api/models/sticker-pack-model.js
@@ -5,15 +5,19 @@ const stickerPackSchema = Schema({
 	name: String,
 	key: {type: String, unique: true},
 	installs: {type: Number, default: 0},
-	creator: {type: Schema.Types.ObjectId, ref: 'User'},
+	creatorId: {type: String, required: true},
 	stickers: [{
 		name: String,
 		url: String,
-		uses: Number,
-		createdAt: Date
+		uses: {type: Number, default: 0},
+		creatorId: {type: String, required: true, default: 'unknown'},
+		createdAt: {type: Date, default: Date.now},	
+		createdVia: {type: String, required: true, default: 'website'},
+		groupType: {type: String, required: true, default: 'sticker-pack'},
+		groupId: {type: String, required: true, default: 'unknown'}
 	}]
 });
 
-let StickerPack = mongoose.model('StickerPack', stickerPackSchema);
+const StickerPack = mongoose.model('StickerPack', stickerPackSchema);
 
 module.exports = StickerPack;
