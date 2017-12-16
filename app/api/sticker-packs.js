c96c50736626ddfbec16a2e9e5c6ba52d8de3c97
DarylPinto <daryl.j.pinto@gmail.com>
2017-12-15 22:42:56 -0500

    Sticker pack api additions + bug fixes

--- app/api/sticker-packs.js
+++ app/api/sticker-packs.js
@@ -22,7 +22,8 @@ let handleMulterError = function(err, req, res, next){
 
 const removedFields = {
 	'_id': false,
-	'__v': false
+	'__v': false,
+	'stickers._id': false
 }
 
 ///////
@@ -61,7 +62,7 @@ router.get('/:key/stickers/:stickername', async (req, res) => {
 	try{
 		const pack = await StickerPack.findOne({key: req.params.key}, removedFields);
 		if(!pack) return res.status(404).send('Sticker Pack not found');
-		const sticker = pack._doc.customStickers.find(s => s.name === req.params.stickername);
+		const sticker = pack.stickers.find(s => s.name === req.params.stickername);
 		if(!sticker) return res.status(404).send('Sticker Pack does not contain a custom sticker with that name');
 		res.json(sticker);
 	}catch(err){
@@ -77,14 +78,17 @@ router.get('/:key/stickers/:stickername', async (req, res) => {
 //POST new sticker pack
 router.post('/', /*verifyUserAjax,*/ async (req, res) => {
 
-	if(!req.body.name || !req.body.key || !req.body.creatorId) return res.status(400).send('Invalid body data');
+	if(!req.body.name || !req.body.key) return res.status(400).send('Invalid body data');
 	if(!req.body.key.match(/^[a-z0-9]+$/g)) return res.status(400).send('Sticker Pack key must contain lowercase letters and numbers only');
-	if(!req.body.key.length > 6) return res.status(400).send('Sticker Pack key cannot be longer than 6 characters');
-	if(!req.body.name.length > 60) return res.status(400).send('Sticker Pack name cannot be longer than 60 characters');
-	//if(res.locals.userId != req.body.creatorId) return res.status(401).send('Unauthorized');
+	if(req.body.key.length > 6) return res.status(400).send('Sticker Pack key cannot be longer than 6 characters');
+	if(req.body.name.length > 60) return res.status(400).send('Sticker Pack name cannot be longer than 60 characters');
+	//if(!res.locals.userId) return res.status(401).send('Unauthorized');
+
+	let data = Object.assign({}, req.body);
+	data.creatorId = 'test-id';//res.locals.userId;
 
 	try{
-		await new StickerPack(req.body).save();
+		await new StickerPack(data).save();
 		const pack = await StickerPack.findOne({key: req.body.key}, removedFields);
 		res.status(201).json(pack);
 	}catch(err){
@@ -101,33 +105,30 @@ router.post('/:key/stickers', /*verifyUserAjax,*/ upload.single('sticker'), hand
 	if(req.body.name.length > 20) return res.status(400).send('Sticker name cannot be longer than 20 characters');	
 	//if(!res.locals.userId) return res.status(401).send('Unauthorized');
 
-	let data = {
+	let sticker = {
 		image: (req.file) ? req.file.buffer : req.body.url,
 		name: req.body.name.toLowerCase().replace(/(:|-)/g, ''),
 		createdVia: (req.file) ? 'website' : 'discord',
 		groupId: req.params.key,
-		//creatorId: res.locals.userId
-	}	
+		creatorId: 'test-id'//res.locals.userId
+	}
 
 	let imageIsLocal = (req.file) ? true : false;
 
 	try{
 
-		let pack = await StickerPack.findOne({key: req.params.key}, removedFields);
+		let pack = await StickerPack.findOne({key: req.params.key});
 		if(!pack) return res.status(404).send('Sticker Pack not found');
 		//if(res.locals.userId != pack.creatorId) return res.status(401).send('Unauthorized');
-		if(pack.stickers.map(s => s.name).includes(data.name)) return res.status(400).send('Sticker Pack already has a custom sticker with that name');
+		if(pack.stickers.map(s => s.name).includes(sticker.name)) return res.status(400).send('Sticker Pack already has a custom sticker with that name');
 		if(pack.stickers.length >= 400) return res.status(403).send('Sticker Pack has reached maximum amount of stickers (400)');
 
-		//let cdn_url = await imageToCdn(data.image, `${pack.key}-${(new Date()).getTime()}-${data.name}`);
-		let cdn_url = 'https://example.com';
+		sticker.url = await imageToCdn(sticker.image, `${pack.key}-${(new Date()).getTime()}-${sticker.name}`);
 
-		let sticker = data;
-		sticker.url = cdn_url;
 		pack.stickers.unshift(sticker);	
-		let saved_pack = await pack.save(); //Error line
+		pack = await pack.save();
 		
-		sticker = saved_pack.stickers.find(s => s.name === data.name);
+		sticker = pack.stickers.find(s => s.name === sticker.name);
 		return res.status(201).json(util.removeProps(sticker._doc, ['_id']));
 
 	}catch(err){
@@ -137,6 +138,21 @@ router.post('/:key/stickers', /*verifyUserAjax,*/ upload.single('sticker'), hand
 		res.status(500).send('Internal server error');
 
 	}
+
+});
+
+//Increment `uses` property on a sticker
+router.post('/:key/stickers/:stickername/uses', /*verifyBot,*/ async (req, res) => {
+
+	let pack = await StickerPack.findOne({key: req.params.key});
+	if(!pack) return res.status(404).send('Sticker Pack not found');
+	let sticker = pack._doc.stickers.find(s => s.name === req.params.stickername);
+	if(!sticker) return res.status(404).send('Sticker Pack does not have a custom sticker with that name');
+
+	sticker.uses += 1;
+	pack.save();
+
+	return res.json(util.removeProps(sticker._doc, ['_id']));
 
 });
 
