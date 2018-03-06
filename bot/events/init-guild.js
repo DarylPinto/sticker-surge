50281a0e98325270e512e77885a6076403184da9
DarylPinto <daryl.j.pinto@gmail.com>
2018-03-06 15:52:25 -0500

    Move `messageDefaultChannel` to it's own utility file

--- bot/events/init-guild.js
+++ bot/events/init-guild.js
@@ -1,40 +1,13 @@
 const rp = require('request-promise');
 const covert = require('../../covert.js');
 const updateGuildInfo = require('../events/update-guild-info.js');
+const messageDefaultChannel = require('../utilities/message-default-channel.js');
 
 module.exports = function(guild, bot_auth){
 	
-	//Send welcome message to #general
-	//If #general doesn't exist, iterate through channels by `calculatedPosition`
-	//And send message to first channel that we have permission to
 	const sendWelcomeMessage = prefix => {
 		let escaped_prefix = prefix.replace(/[^a-zA-Z0-9]/g, '\\$&');
-
-		let channels = guild.channels.array().filter(c => c.type === 'text');
-		let general_channel = (channels.map(c => c.name).includes('general')) ? channels[channels.map(c => c.name).indexOf('general')] : null;
-
-		let sorted_channels = channels.sort((a, b) => {
-			if(a.calculatedPosition < b.calculatedPosition) return -1;
-			else if(b.calculatedPosition < a.calculatedPosition) return 1;
-			else return 0;
-		});
-
-		if(general_channel) sorted_channels.unshift(general_channel);
-
-		let channel_index = 0;
-
-		function attemptWelcome(){
-			sorted_channels[channel_index].send(`
-				Your server can now use stickers!
-				Type **${escaped_prefix}help** to get started.
-			`.replace(/\t+/g, ''))	
-			.catch(err => {
-				channel_index++;
-				if(channel_index < sorted_channels.length) attemptWelcome();
-			});
-		}
-
-		attemptWelcome();
+		messageDefaultChannel(guild, `This server can now use stickers! Type **${escaped_prefix}help** to get started.`);	
 	}
 
 	rp({
