408bd8a47bb703493aa176471d8dc4dd0097b792
Daryl Pinto <daryl.j.pinto@gmail.com>
2018-04-23 10:29:16 -0400

    Add global flag for DBL integration

--- bot/events/update-dbl-stats.js
+++ bot/events/update-dbl-stats.js
@@ -4,7 +4,7 @@ const covert = require('../../covert.js');
 //Update guild count on Discord Bot List
 module.exports = function(client){
 
-	if(!covert.discord_bot_list.api_key) return false;
+	if(!covert.discord_bot_list.integrated || !covert.discord_bot_list.api_key) return false;	
 	const dbl = new DBL(covert.discord_bot_list.api_key);
 
 	dbl.postStats(client.guilds.size);
