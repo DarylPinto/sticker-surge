3624eeaf85cd474f91351145cf39652beea62e2d
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-06-18 15:29:17 -0400

    Check channel permissions instead of guild permissions for bot actions

--- bot/events/send-sticker.js
+++ bot/events/send-sticker.js
@@ -100,10 +100,10 @@ module.exports = async function(message, client, bot_auth){
 				}
 
 				//Delete original message
-				if(message.guild.me.hasPermission('MANAGE_MESSAGES')) message.delete();
+				if(message.channel.memberPermissions(client.user).has('MANAGE_MESSAGES')) message.delete();
 
 				//Webhook style sticker
-				if(message.guild.me.hasPermission('MANAGE_WEBHOOKS')){
+				if(message.channel.memberPermissions(client.user).has('MANAGE_WEBHOOKS')){
 					let hook = await getStickerWebhook(message.channel);
 					//Discord requires webhook names to be 2 chars minimum.
 					//Spread operator below provides more accurate char count for usernames that can potentially include emojis	
