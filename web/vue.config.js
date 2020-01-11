145bef8fb9e59d9a3a079de5dadba843c059c99b
Daryl Pinto <daryl.j.pinto@gmail.com>
2020-01-11 17:59:54 -0500

    Fix favicon

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,3 +1,25 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  pwa: {
+    name: 'Stickers for Discord',
+    themeColor: '#FC6262',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
+    },
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 };
