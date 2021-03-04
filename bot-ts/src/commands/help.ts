33cec7f081982e501f9e502110fc03afbd664354
Daryl Pinto <daryl.j.pinto@gmail.com>
2021-03-03 22:18:22 -0500

    fix: could not destructure commandPrefix from undefined

--- bot-ts/src/commands/help.ts
+++ bot-ts/src/commands/help.ts
@@ -1,6 +1,6 @@
 import { Command } from "discord-akairo";
 import { Message } from "discord.js";
-import stickerSurge from "../services/stickerSurge";
+import stickerSurge, { Status } from "../services/stickerSurge";
 import respondToCommand from "../utils/respondToCommand";
 import { EMBED_COLOR } from "../data/constants";
 import escapePrefix from "../utils/escapePrefix";
@@ -29,54 +29,55 @@ class HelpCommand extends Command {
     let responseMessage = "";
     let responseMessageOptions = {};
 
-    try {
-      const {
-        commandPrefix,
-        listMode,
-        whitelist: { roleId: whitelistRoleId },
-        blacklist: { roleId: blacklistRoleId },
-        stickerManagers: { roleId: stickerManagerRoleId },
-        personalStickersAllowed,
-        customStickers,
-      } = result.data;
+    if (result.status === Status.SUCCESS) {
+      try {
+        const {
+          commandPrefix,
+          listMode,
+          whitelist: { roleId: whitelistRoleId },
+          blacklist: { roleId: blacklistRoleId },
+          stickerManagers: { roleId: stickerManagerRoleId },
+          personalStickersAllowed,
+          customStickers,
+        } = result.data;
 
-      const escapedPrefix = escapePrefix(commandPrefix);
-      const roleManager = message.guild.roles;
+        const escapedPrefix = escapePrefix(commandPrefix);
+        const roleManager = message.guild.roles;
 
-      const stickerManagerRoleName =
-        stickerManagerRoleId === "@everyone"
-          ? "Everyone"
-          : roleManager.resolve(stickerManagerRoleId).name;
-
-      let whoCanSend;
-      if (listMode === "whitelist") {
-        whoCanSend =
-          whitelistRoleId === "@everyone"
+        const stickerManagerRoleName =
+          stickerManagerRoleId === "@everyone"
             ? "Everyone"
-            : `Anyone with the role *${
-                roleManager.resolve(whitelistRoleId).name
-              }*`;
-      } else {
-        const blacklistRoleName = roleManager.resolve(blacklistRoleId).name;
-        whoCanSend = `Anyone __without__ the role *${blacklistRoleName}*`;
-      }
+            : roleManager.resolve(stickerManagerRoleId).name;
 
-      responseMessageOptions = {
-        embed: {
-          color: EMBED_COLOR,
-          fields: [
-            {
-              name: "Sticker Surge",
-              value: `
+        let whoCanSend;
+        if (listMode === "whitelist") {
+          whoCanSend =
+            whitelistRoleId === "@everyone"
+              ? "Everyone"
+              : `Anyone with the role *${
+                  roleManager.resolve(whitelistRoleId).name
+                }*`;
+        } else {
+          const blacklistRoleName = roleManager.resolve(blacklistRoleId).name;
+          whoCanSend = `Anyone __without__ the role *${blacklistRoleName}*`;
+        }
+
+        responseMessageOptions = {
+          embed: {
+            color: EMBED_COLOR,
+            fields: [
+              {
+                name: "Sticker Surge",
+                value: `
 						To view a list of commands, type: **${escapedPrefix}commands**
 						[View available sticker packs](${process.env.APP_URL}/sticker-packs)
 						[View guide](${process.env.APP_URL}/docs)
 						.
 					`.replace(/\t/g, ""),
-            },
-            {
-              name: message.guild.name,
-              value: `
+              },
+              {
+                name: message.guild.name,
+                value: `
 						Command Prefix: ${escapedPrefix}
 						Who can use stickers: ${whoCanSend}
 						Sticker Manager Role: ${stickerManagerRoleName}
@@ -85,33 +86,34 @@ class HelpCommand extends Command {
 						[View Stickers](${process.env.APP_URL}/server/${message.guild.id})
 						.
 					`.replace(/\t/g, ""),
-            },
-            {
-              name: "Misc. Links",
-              value: `
+              },
+              {
+                name: "Misc. Links",
+                value: `
 						[Join our Discord](${discordLink})
 						[Add bot to another server](${botInviteLink})
 						[Enjoy the bot? Upvote it on top.gg <3](${botVoteLink})
 						___
 					`.replace(/\t/g, ""),
-            },
-          ],
-          footer: { text: "Created by: DRL#1287" },
-        },
-      };
-    } catch (err) {
-      logger.error({
-        message: "Error occured while generating *help* command message",
-        meta: {
-          error: { message: err.message, stack: err.stack },
-          botCommand: COMMAND_NAME,
-          guild: message.guild.id,
-          user: message.author.id,
-          message: { id: message.id, content: message.content },
-        },
-      });
+              },
+            ],
+            footer: { text: "Created by: DRL#1287" },
+          },
+        };
+      } catch (err) {
+        logger.error({
+          message: "Error occured while generating *help* command message",
+          meta: {
+            error: { message: err.message, stack: err.stack },
+            botCommand: COMMAND_NAME,
+            guild: message.guild.id,
+            user: message.author.id,
+            message: { id: message.id, content: message.content },
+          },
+        });
 
-      responseMessage = "Something unexpected happened";
+        responseMessage = "Something unexpected happened";
+      }
     }
 
     await respondToCommand({
